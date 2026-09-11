f00e917579e61186866d679db719c259293a2326
decoelectroclima-ctrl <decoelectroclima@gmail.com>
2026-09-11 16:35:24 +0200

    build: bump version to 230.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.aistudio.atalaya.kxmpzq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 229
-    versionName = "229.0"
+    versionCode = 230
+    versionName = "230.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
