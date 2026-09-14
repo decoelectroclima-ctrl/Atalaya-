f703d1198d09cc36ffb7f5a415754e5d6b32bbd7
decoelectroclima-ctrl <decoelectroclima@gmail.com>
2026-09-14 13:56:03 +0200

    build: bump version to 268.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.aistudio.atalaya.kxmpzq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 266
-    versionName = "266.0"
+    versionCode = 268
+    versionName = "268.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
